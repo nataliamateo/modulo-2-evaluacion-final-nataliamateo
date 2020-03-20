26b8ed2aee15e6cc011e1a4c9dd911164099d6d5
Natalia Mateo <nmateo7@gmail.com>
2020-03-20 12:08:08 +0100

    Create functions GetdataApi and paintShows and too change class names on html

--- js/main.js
+++ js/main.js
@@ -1,15 +1,50 @@
 "use strict";
 
-let products = [];
-let favorites = [];
+let shows = [];
+let favoritesShows = [];
 
-function listenFoundDataBtn() {}
+const btn = document.querySelector(".js-button");
+btn.addEventListener("click", getDataApi);
+
 // que escuche el botón
 
-function getDataApi() {}
 // función que tome los datos del servidor con el valor del input
+function getDataApi() {
+  const inputValue = document.querySelector(".js-input").value;
+  fetch(`http://api.tvmaze.com/search/shows?q=${inputValue}`)
+    .then(response => response.json())
+    .then(serverData => {
+      /* necesito recorrer el array de 10 objetos que devuelve el servidor para acceder 
+      a los datos que necesito. Como devuelve 10 objetos tengo que crear 1 con las propiedades que quiero que muestre */
+      for (const show of serverData) {
+        const filmsDataObject = {
+          id: show.show.id,
+          name: show.show.name,
+          image: show.show.image
+        };
+        if (show.show.image === null) {
+          filmsDataObject.image = "https://via.placeholder.com/210x295/ffffff/666666/?text=TV";
+        } else {
+          filmsDataObject.image = show.show.image.medium;
+        }
+        shows.push(filmsDataObject);
+      }
+      paintShows();
+      console.log(shows);
+    });
+}
 
-function paintData() {}
+const showsElement = document.querySelector(".js-container-shows");
+const paintShows = () => {
+  let htmlCode = "";
+  for (const show of shows) {
+    htmlCode += `<div class="container_shows-show" id="${show.id}">`;
+    htmlCode += `<img src="${show.image}" alt="Serie: ${show.name}" class="image-style">`;
+    htmlCode += `<h3>${show.name}</h3>`;
+    htmlCode += `</div>`;
+  }
+  showsElement.innerHTML = htmlCode;
+};
 // función que pinta los datos de la API
 
 function paintFavoriteData() {}
