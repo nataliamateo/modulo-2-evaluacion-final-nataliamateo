892bb72dad0ca58b9e20c59c53388cc8de1d0ff8
Natalia Mateo <nmateo7@gmail.com>
2020-03-20 08:36:50 +0100

    Create folders and records

---
+++ js/main.js
@@ -0,0 +1,21 @@
+"use strict";
+
+let products = [];
+let favorites = [];
+
+function listenFoundDataBtn() {}
+// que escuche el botón
+
+function getDataApi() {}
+// función que tome los datos del servidor con el valor del input
+
+function paintData() {}
+// función que pinta los datos de la API
+
+function paintFavoriteData() {}
+// función que pinte los datos de la lista de favorito
+function favouriteList() {}
+// un función que añada o quite de la lista de favoritos
+
+function setInLocalStorage() {}
+function getFromLocalStorage() {}
